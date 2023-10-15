935f00a0bb1e86998f32da3eaf8054d79cfbdab7
FernandoHdez67 <20200767@uthh.edu.mx>
2023-10-15 16:35:21 -0600

    act

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,67 +1,62 @@
-// Define el nombre de la caché estática
 var staticCacheName = "pwa-v" + new Date().getTime();
+var filesToCache = [
+    '/offline',
+    '/css/app.css',
+    '/js/app.js',
+    '/images/icons/icon-72x72.png',
+    '/images/icons/icon-96x96.png',
+    '/images/icons/icon-128x128.png',
+    '/images/icons/icon-144x144.png',
+    '/images/icons/icon-152x152.png',
+    '/images/icons/icon-192x192.png',
+    '/images/icons/icon-384x384.png',
+    '/images/icons/icon-512x512.png',
+    '/images/icons/splash-640x1136.png',
+    '/images/icons/splash-750x1334.png',
+    '/images/icons/splash-1242x2208.png',
+    '/images/icons/splash-1125x2436.png',
+    '/images/icons/splash-828x1792.png',
+    '/images/icons/splash-1242x2688.png',
+    '/images/icons/splash-1536x2048.png',
+    '/images/icons/splash-1668x2224.png',
+    '/images/icons/splash-1668x2388.png',
+    '/images/icons/splash-2048x2732.png'
+];
 
-// Importa el módulo de Workbox.
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.5/workbox-sw.js');
-
-// Usa las estrategias de Workbox para cachear los recursos.
-const { precacheAndRoute } = workbox.precaching;
-const { CacheFirst, NetworkFirst } = workbox.strategies;
-const { registerRoute } = workbox.routing;
-
-// Lista de archivos que se cachearán durante la instalación.
-precacheAndRoute([
-  '/resources/views/vendor/laravelpwa/offline.blade.php',
-  '/css/bootstrap.min.css',
-  '/css/bootstrap.min.js',
-  '/mystyle/mystyle.css',
-  '/offline',
-  '/images/icons/icon-72x72.png',
-  '/images/icons/icon-96x96.png',
-  '/images/icons/icon-128x128.png',
-  '/images/icons/icon-144x144.png',
-  '/images/icons/icon-152x152.png',
-  '/images/icons/icon-192x192.png',
-  '/images/icons/icon-384x384.png',
-  '/images/icons/icon-512x512.png',
-]);
-
-// Cachea archivos de imagen con CacheFirst.
-registerRoute(/\.(png|jpg|jpeg|gif)$/, new CacheFirst());
-
-// Cachea datos JSON con NetworkFirst.
-registerRoute(/\.json$/, new NetworkFirst());
-
-// Cachea una ruta específica con una estrategia personalizada.
-registerRoute("/api/data", async ({ event }) => {
-  // ... (lógica de caché personalizada aquí)
+// Cache on install
+self.addEventListener("install", event => {
+    this.skipWaiting();
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then(cache => {
+                return cache.addAll(filesToCache);
+            })
+    )
 });
 
 // Clear cache on activate
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [staticCacheName];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(cacheName => (cacheName.startsWith("pwa-")))
+                    .filter(cacheName => (cacheName !== staticCacheName))
+                    .map(cacheName => caches.delete(cacheName))
+            );
         })
-      );
-    })
-  );
+    );
 });
 
-// Serve from Cache using Workbox strategies
+// Serve from Cache
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-      .catch(() => {
-        return caches.match('/offline');
-      })
-  );
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+            .catch(() => {
+                return caches.match('offline');
+            })
+    )
 });
