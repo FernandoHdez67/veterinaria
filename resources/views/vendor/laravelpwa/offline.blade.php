3117e4789d752132013b80e648ef17467fe49e25
FernandoHdez67 <20200767@uthh.edu.mx>
2023-10-16 15:32:15 -0600

    splash screen

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -1,8 +1,9 @@
 @extends('principal')
 
 @section('contenido')
-
+    <br><br><br><br><br>
     <center><h1>Actualmente no estás conectado a ninguna red.</h1></center>
+    <img src="{{ asset('img/no-wifi.png') }}" width="250px" height="250px" alt="no-wifi">
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 @endsection
