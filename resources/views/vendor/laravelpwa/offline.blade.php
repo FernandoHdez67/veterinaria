426271d143a9fb5f61af225ead6b6b713a8875e5
FernandoHdez67 <20200767@uthh.edu.mx>
2023-10-15 22:32:13 -0600

    pwa-access

--- resources/views/vendor/laravelpwa/offline.blade.php
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -2,7 +2,7 @@
 
 @section('contenido')
 
-    <h1>Actualmente no estás conectado a ninguna red.</h1>
-    <br><br><br><br><br><br>
+    <center><h1>Actualmente no estás conectado a ninguna red.</h1></center>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 @endsection
